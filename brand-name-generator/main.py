55ef05b98643f8185689a9de4858f11b96f47c5e
shashank shet <shashankshetk@gmail.com>
2022-01-10 16:30:29 +0530

    Day 1

---
+++ brand-name-generator/main.py
@@ -0,0 +1,9 @@
+print("Welcome to Brand name generator Application!")
+
+city_name = input("please enter the city you grew up in: ")
+
+pet_name = input("please enter your pet name: ")
+
+brand_name = city_name + " " + pet_name
+
+print("your brand name could be "+brand_name)
